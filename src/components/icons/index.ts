033974c1d0446b48cc8404d78cf819aa979b31fb
Your Name <you@example.com>
2024-11-23 15:28:17 +0330

    add phone number icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,3 +2,4 @@ export { default as ExternalLink } from "./external-link";
 export { default as Github } from "./github";
 export { default as LinkedIn } from "./linkedin";
 export { default as Mail } from "./mail";
+export { default as Phone } from "./phone";
